32ec4b4595f68f06ebc24eb3f5715c972a7daf05
Nemo3003 <ulimarian2000@gmail.com>
2022-09-26 11:18:46 -0300

    [UPD] Few fixes made

--- controllers/contacts.controllers.js
+++ controllers/contacts.controllers.js
@@ -1,14 +1,18 @@
 //@TS-Check
 const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
+
 //-***********************************************************************************************
+
 const getAllContacts = async (req, res) => {
   const result = await mongodb.getDb().db('cse341').collection("contacts").find().toArray().then((result) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(result);
   });
 };
+
 //-***********************************************************************************************
+
 const getContactById = async (req, res) => {
   const contactId = new ObjectId(req.params.id);
   const result = await mongodb.getDb().db('cse341').collection("contacts").find({ _id: contactId }).toArray().then((result) => {
@@ -16,7 +20,9 @@ const getContactById = async (req, res) => {
     res.status(200).json(result);
   });
 };
+
 //-***********************************************************************************************
+
 const createContact = async (req, res) => {
   
   const contact = {
@@ -33,16 +39,21 @@ const createContact = async (req, res) => {
     res.status(500).json(response.error || 'Some error occurred while creating the contact.');
   }
 };
+
 //-***********************************************************************************************
+
 const deleteContact = async (req, res) => {
   const contactId = new ObjectId(req.params.id);
+  const resPonder = 200
   try {
     await mongodb.getDb().db('cse341').collection('contacts').deleteOne({ _id: contactId }, true);
-    res.status(204).send;
+    res.sendStatus(200);
   } catch (error) {
      res.status(500).send;
   }};
+
 //-***********************************************************************************************
+
 const updateContact = async (req, res) => {
   const userId = new ObjectId(req.params.id);
   // be aware of updateOne if you only want to update specific fields
