6e65d994f384d73f36ccdf5a7c4a660e0b784cb4
Nemo3003 <ulimarian2000@gmail.com>
2022-10-05 09:29:13 -0300

    [UPD] Cors added

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 //@TS-Check
 const express = require('express');
+const cors = require('cors');
 const app = express();
 app.use(express.json());
 const PORT = 8080;
 const mongodb = require('./db/connect');
 
-app.use('/', require('./routes'));
+app
+  .use(cors())
+  .use(express.json())
+  .use(express.urlencoded({ extended: true }))
+  .use('/', require('./routes'));
 
 // Main
 mongodb.initDb((err, mongodb) => {
