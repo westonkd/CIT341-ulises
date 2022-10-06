5ad194d780b0c48731baac3c1a0b3cf7a602264d
Nemo3003 <ulimarian2000@gmail.com>
2022-10-06 12:06:14 -0300

    [UPD] Cors added to routes and whitelist

--- routes/contacts.js
+++ routes/contacts.js
@@ -11,16 +11,28 @@ const {
     deleteContact
 } = require('../controllers/contacts.controllers');
 
+var whitelist = ['https://cse341-ulises.onrender.com/']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+ 
+
 // Main
-router.get('/', cors(), getAllContacts);
+router.get('/', cors(corsOptions), getAllContacts);
 
-router.get('/:id',cors(), getContactById);
+router.get('/:id',cors(corsOptions), getContactById);
 
-router.post('/',cors(), createContact)
+router.post('/',cors(corsOptions), createContact)
 
-router.put('/:id',cors(), updateContact);
+router.put('/:id',cors(corsOptions), updateContact);
 
-router.delete('/:id',cors(), deleteContact);
+router.delete('/:id',cors(corsOptions), deleteContact);
 
 // Exports
 module.exports = router;
