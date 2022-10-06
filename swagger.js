df4939b8dc1823633d2a2454c93ca5ce7211fb5a
Nemo3003 <ulimarian2000@gmail.com>
2022-10-06 19:29:33 -0300

    [UPD] another change

--- swagger.js
+++ swagger.js
@@ -10,7 +10,7 @@ const doc = {
 };
 
 const outputFile = './routes/swagger.json';
-const endpointsFiles = ['./routes/index.js'];
+const endpointsFiles = ['./index.js'];
 
 // generate swagger.json
 swaggerAutogen(outputFile, endpointsFiles, doc);
