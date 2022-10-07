acea57a89f02824a6810de973254ba6f03662277
Weston Dransfield <wdrans@gmail.com>
2022-10-06 18:10:35 -0600

    set port

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 //@TS-Check
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 app.use(express.json());
-const PORT = 8080;
-const mongodb = require('./db/connect');
+const PORT = process.env.PORT;
+const mongodb = require("./db/connect");
 
 app
   .use(cors())
   .use(express.json())
   .use(express.urlencoded({ extended: true }))
-  .use('/', require('./routes'));
+  .use("/", require("./routes"));
 
 // Main
 mongodb.initDb((err, mongodb) => {
