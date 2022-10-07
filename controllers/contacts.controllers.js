08b021ad4ffa41f43388ff339145e35911f4f729
Weston Dransfield <wdrans@gmail.com>
2022-10-06 18:04:58 -0600

    udpate db name

--- controllers/contacts.controllers.js
+++ controllers/contacts.controllers.js
@@ -1,42 +1,61 @@
 //@TS-Check
-const mongodb = require('../db/connect');
-const ObjectId = require('mongodb').ObjectId;
+const mongodb = require("../db/connect");
+const ObjectId = require("mongodb").ObjectId;
 
 //-***********************************************************************************************
 
 const getAllContacts = async (req, res) => {
-  const result = await mongodb.getDb().db('cse341').collection("contacts").find().toArray().then((result) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(result);
-  });
+  const result = await mongodb
+    .getDb()
+    .db("cs_341_projects")
+    .collection("contacts")
+    .find()
+    .toArray()
+    .then((result) => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).json(result);
+    });
 };
 
 //-***********************************************************************************************
 
 const getContactById = async (req, res) => {
   const contactId = new ObjectId(req.params.id);
-  const result = await mongodb.getDb().db('cse341').collection("contacts").find({ _id: contactId }).toArray().then((result) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(result);
-  });
+  const result = await mongodb
+    .getDb()
+    .db("cs_341_projects")
+    .collection("contacts")
+    .find({ _id: contactId })
+    .toArray()
+    .then((result) => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).json(result);
+    });
 };
 
 //-***********************************************************************************************
 
 const createContact = async (req, res) => {
-  
   const contact = {
     firstName: req.body.firstName,
     lastName: req.body.lastName,
     email: req.body.email,
     favoriteColor: req.body.favoriteColor,
-    birthday: req.body.birthday
+    birthday: req.body.birthday,
   };
-  const response = await mongodb.getDb().db('cse341').collection('contacts').insertOne(contact);
+  const response = await mongodb
+    .getDb()
+    .db("cs_341_projects")
+    .collection("contacts")
+    .insertOne(contact);
   if (response.acknowledged) {
     res.status(201).json(response);
   } else {
-    res.status(500).json(response.error || 'Some error occurred while creating the contact.');
+    res
+      .status(500)
+      .json(
+        response.error || "Some error occurred while creating the contact."
+      );
   }
 };
 
@@ -44,13 +63,18 @@ const createContact = async (req, res) => {
 
 const deleteContact = async (req, res) => {
   const contactId = new ObjectId(req.params.id);
-  const resPonder = 200
+  const resPonder = 200;
   try {
-    await mongodb.getDb().db('cse341').collection('contacts').deleteOne({ _id: contactId }, true);
+    await mongodb
+      .getDb()
+      .db("cs_341_projects")
+      .collection("contacts")
+      .deleteOne({ _id: contactId }, true);
     res.sendStatus(200);
   } catch (error) {
-     res.status(500).send;
-  }};
+    res.status(500).send;
+  }
+};
 
 //-***********************************************************************************************
 
@@ -62,26 +86,30 @@ const updateContact = async (req, res) => {
     lastName: req.body.lastName,
     email: req.body.email,
     favoriteColor: req.body.favoriteColor,
-    birthday: req.body.birthday
+    birthday: req.body.birthday,
   };
   const response = await mongodb
     .getDb()
-    .db('cse341')
-    .collection('contacts')
+    .db("cs_341_projects")
+    .collection("contacts")
     .replaceOne({ _id: userId }, contact);
   console.log(response);
   if (response.modifiedCount > 0) {
     res.status(204).send();
   } else {
-    res.status(500).json(response.error || 'Some error occurred while updating the contact.');
+    res
+      .status(500)
+      .json(
+        response.error || "Some error occurred while updating the contact."
+      );
   }
 };
 
 // Exports
-module.exports = { 
-   getAllContacts,
-   getContactById, 
-   createContact,
-   deleteContact,
-   updateContact
-  };
+module.exports = {
+  getAllContacts,
+  getContactById,
+  createContact,
+  deleteContact,
+  updateContact,
+};
